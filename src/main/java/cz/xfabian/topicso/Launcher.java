7ea53fd99fc5b883b88acafd7a4c477a9527c641
Norbert Fabian <fabian.norbert@hotmail.sk>
2020-04-06 10:53:37 +0200

    Change DbFiller to EventListener based DbSeeder

--- src/main/java/cz/xfabian/topicso/Launcher.java
+++ src/main/java/cz/xfabian/topicso/Launcher.java
@@ -2,13 +2,11 @@ package cz.xfabian.topicso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Launcher {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(Launcher.class, args);
-		context.getBean(DataFiller.class).fillData();
+		SpringApplication.run(Launcher.class, args);
 	}
 }
--- src/main/java/cz/xfabian/topicso/persistence/seed/DbSeeder.java
+++ src/main/java/cz/xfabian/topicso/persistence/seed/DbSeeder.java
@@ -1,17 +1,24 @@
-package cz.xfabian.topicso;
+package cz.xfabian.topicso.persistence.seed;
 
 import cz.xfabian.topicso.core.video.VideoService;
 import cz.xfabian.topicso.rest.video.dto.VideoDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DataFiller {
+public class DbSeeder {
 
     @Autowired
     private VideoService videoService;
 
-    public void fillData() {
+    @EventListener
+    public void seed(ContextRefreshedEvent event) {
+        seedVideos();
+    }
+
+    private void seedVideos() {
         VideoDTO videoDTO = new VideoDTO(1, "JUnit 5 Basics 1 - Introduction and agenda", "2E3WqYupx7c", 5, "JUnit 5 Basics is an introduction to the JUnit Jupiter testing framework. JUnit is the defacto standard for testing in Java. learn about how to use JUnit to write effective tests. Understand the features of JUnit including the `@Test` annotation, assertion APIs, test lifecycle and controlling executions.");
         videoService.addVideo(videoDTO);
 
@@ -30,5 +37,4 @@ public class DataFiller {
         videoDTO = new VideoDTO(6, "JUnit 5 Basics 6 - Creating a new JUnit 5 Maven project", "09ZZiNgUPVU", 4.8, "JUnit 5 Basics is an introduction to the JUnit Jupiter testing framework. JUnit is the defacto standard for testing in Java. learn about how to use JUnit to write effective tests. Understand the features of JUnit including the `@Test` annotation, assertion APIs, test lifecycle and controlling executions.");
         videoService.addVideo(videoDTO);
     }
-
 }
